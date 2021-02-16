a15c39f38e9f83f3cc1a21d3a955dbea9d0a7f84
peterino <peterlimail47@gmail.com>
2021-02-16 03:03:39 -0800

    module version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
     setuptools.setup(
         name="pogmake",  # Replace with your own username
-        version="0.0.1",
+        version="0.0.2",
         scripts=['pog', 'pogm', 'pog.bat'],
         author="Peter Li",
         author_email="peterlimail47@gmail.com",
